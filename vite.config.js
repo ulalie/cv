3a6b715eb395e27fed17bd5cd3de9f007922c719
ulalie <ulanapynkina05.09@gmail.com>
2024-12-22 02:20:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"https://ulalie.github.io/assets/cv"
   css: {
     modules:{
       localsConvention: "camelCase"
